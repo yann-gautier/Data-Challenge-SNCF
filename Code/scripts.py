0cf90f87254d499482dc8184e5f1b73abb308458
Yannoulefou <yannou.gautier.12@gmail.com>
2025-08-12 14:11:41 +0200

    commit

--- Code/scripts.py
+++ Code/scripts.py
@@ -4,23 +4,19 @@ from sklearn.metrics import accuracy_score, classification_report
 import lightgbm as lgb
 
 # 1. Charger les données
-df = pd.read_csv("donnees.csv")
 
-# 2. Séparer features et target
-X = df.drop(columns=["retard_minutes"])
-y = df["retard_minutes"]
-
-X = pd.read_csv()
-y = pd.read_csv()
+X = pd.read_csv("C:/Users/yanno/OneDrive/Bureau/Data Challenge SNCF/Données/x_train_final.csv")
+y = pd.read_csv("C:/Users/yanno/OneDrive/Bureau/Data Challenge SNCF/Données/y_train_final_j5KGWWK.csv")["p0q0"]
+X_test= pd.read_csv("C:/Users/yanno/OneDrive/Bureau/Data Challenge SNCF/Données/x_test_final.csv")
 
 # 3. Train/test split
-X_train, X_test, y_train, y_test = train_test_split(
+X_train, X_ev, y_train, y_ev = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
 # 4. Dataset LightGBM
 train_data = lgb.Dataset(X_train, label=y_train)
-test_data = lgb.Dataset(X_test, label=y_test)
+ev_data = lgb.Dataset(X_ev, label=y_ev)
 
 # 5. Paramètres LightGBM
 params = {
@@ -37,7 +33,7 @@ params = {
 model = lgb.train(
     params,
     train_data,
-    valid_sets=[train_data, test_data],
+    valid_sets=[train_data, ev_data],
     num_boost_round=1000,
     early_stopping_rounds=50,
     verbose_eval=100
@@ -46,7 +42,3 @@ model = lgb.train(
 # 7. Prédictions
 y_pred_proba = model.predict(X_test)  # probabilités par classe
 y_pred = y_pred_proba.argmax(axis=1)  # choisir classe la plus probable
-
-# 8. Évaluation
-print("Accuracy:", accuracy_score(y_test, y_pred))
-print(classification_report(y_test, y_pred))
